246ab30180028f3482a6d327c68bca5446c87852
Debajyoti Nag <dave0908@gmail.com>
2014-02-09 17:25:13 +0100

    main module modified to create seed network, and display CGPA

--- thesis/matlab/Main.m
+++ thesis/matlab/Main.m
@@ -1,7 +1,10 @@
-function [s, c, weak, strong] = Main( Num_Agents )
+function [s, c, weak, strong, probability] = Main( Num_Agents )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-s = createAgents(Num_Agents);
-[weak, strong, c, s] = createConnections(s , Num_Agents);
+[s, c, weak, strong] = Seed(Num_Agents);
+
+k = length(s);
+s(k+1) = addAgent(Num_Agents+1 , 2, 0.5, 0.5);
+probability = findProbability(s(k+1), s, c);
 end
